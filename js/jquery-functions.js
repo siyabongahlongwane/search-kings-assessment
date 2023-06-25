d04aee4186a51ac7f091e65a80e56aed66f57fda
Siyabonga <hlongwanesiyabonga6@gmail.com>
2023-06-26 01:47:21 +0200

    Responsive Navigation, Toggle Icon Function

--- js/jquery-functions.js
+++ js/jquery-functions.js
@@ -4,16 +4,8 @@ $('li.dropdown').hover(function() {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
 
-$(document).ready(function() {
-    $('.dropdown').on('click', function(e) {
-        e.stopPropagation();
-        var $dropdownMenu = $(this).find('.dropdown-menu');
-        $dropdownMenu.toggleClass('show');
-    });
-
-    $(document).on('click', function(e) {
-        if (!$('.dropdown').is(e.target) && $('.dropdown').has(e.target).length === 0) {
-            $('.dropdown-menu').removeClass('show');
-        }
-    });
+$('i.navbar-toggler').on('click', function() {
+    $("i.navbar-toggler").fadeToggle(200);
+    $("i.navbar-toggler").toggleClass('fa fa-bars').toggleClass('fa fa-times-thin fa-3x');
+    open = false;
 });
